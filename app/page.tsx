247b4f6e8707911c7248b19e244332ac8b42079a
Yira Gallardo <yiramartinez04@gmail.com>
2024-02-29 01:14:26 -0600

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -237,8 +237,8 @@ export default function Home() {
             <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="30"
-                height="30"
+                width="32"
+                height="32"
                 fill="currentColor"
                 viewBox="0 0 16 16"
               >
