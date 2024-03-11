0d5052b18721d2ba7ae2aa4db6f645a060ec65a5
Yira Gallardo <yiramartinez04@gmail.com>
2024-03-11 00:32:54 -0600

    fix aciso de privacidad

--- app/page.tsx
+++ app/page.tsx
@@ -18,20 +18,6 @@ export default function Home() {
     telefono: "",
   });
 
-  useEffect(() => {
-    const hora = new Date().getHours();
-    if (hora >= 6 && hora < 18) {
-      document.body.classList.add("dia");
-    } else {
-      document.body.classList.add("noche");
-    }
-
-    // Opcional: Limpiar las clases al desmontar el componente
-    return () => {
-      document.body.classList.remove("dia", "noche");
-    };
-  }, []);
-
   const handleChange = (e: any) => {
     const { name, value } = e.target;
     setFormData((prevState: any) => ({
@@ -56,7 +42,7 @@ export default function Home() {
       .then(
         () => {
           console.log("result.text");
-          setSubmitMessage("Se envió correctamente");
+          setSubmitMessage("Gracias, hemos recibido tu información");
           setMandandoMensaje(false);
         },
         (error) => {
@@ -147,7 +133,7 @@ export default function Home() {
 
         <div className="overflow-hidden">
           <video loop className="w-full" controls>
-            <source src="/img/la-fiesta.mp4" type="video/mp4" />
+            <source src="/img/video-final.mp4" type="video/mp4" />
           </video>
         </div>
         <div className="container mx-auto py-20">
@@ -301,6 +287,24 @@ export default function Home() {
                 className="icon"
               >
                 <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
+              </svg>
+            </div>
+          </Link>
+          <Link
+            href="https://wa.me/5579223842"
+            target="_blank"
+            className="flex justify-center items-center gap-2 "
+          >
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                fill="currentColor"
+                className="icon"
+                viewBox="0 0 16 16"
+              >
+                <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
               </svg>
             </div>
           </Link>
